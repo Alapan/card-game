b09d438063da30aa6818c28df7f1c80a6897abb5
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2024-12-07 19:06:42 +0000

    Update image files

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,10 @@
 export const initialCardImages: CardImage[] = [
   { src: '/img/bag-1.png', isFaceDown: true },
-  { src: '/img/bank-1.png', isFaceDown: true },
-  { src: '/img/horse-1.png', isFaceDown: true },
-  { src: '/img/money-1.png', isFaceDown: true },
-  { src: '/img/piggy-1.png', isFaceDown: true },
-  { src: '/img/pound-1.png', isFaceDown: true },
+  { src: '/img/bone-1.png', isFaceDown: true },
+  { src: '/img/car-1.png', isFaceDown: true },
+  { src: '/img/coin-1.png', isFaceDown: true },
+  { src: '/img/earth-1.png', isFaceDown: true },
+  { src: '/img/sun-1.png', isFaceDown: true },
 ];
 
 export interface CardImage {
